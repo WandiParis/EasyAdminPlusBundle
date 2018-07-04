bfa1dc73e25b50ab08abca8525946324a0f5b16d
sebastien <2le@2le.net>
2018-07-04 17:24:00 +0200

    ajout UrlAutocompleteType

---
+++ src/Form/Type/UrlAutocompleteType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lle\EasyAdminPlusBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+class UrlAutocompleteType extends AbstractType
+{
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    public function getName()
+    {
+        return 'lle.easyadminplus.url_autocomplete';
+    }
+
+    public function configureOptions(OptionsResolver $resolver){
+        $resolver->setDefaults([
+            "url" => "#",
+            "value_filter" => ''
+        ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options){
+        $view->vars['url'] = $options['url'];
+        $view->vars['value_filter'] = $options['value_filter'];
+    }    
+}
