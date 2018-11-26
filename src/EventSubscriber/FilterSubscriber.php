2949df778977207ecff851e2c0542664e1adf5db
Jérôme <jerome@2le.net>
2018-11-26 11:17:49 +0100

    Fix check extends AdminController

--- src/EventSubscriber/FilterSubscriber.php
+++ src/EventSubscriber/FilterSubscriber.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Lle\EasyAdminPlusBundle\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -7,41 +6,49 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Lle\EasyAdminPlusBundle\Filter\FilterState;
 use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Lle\EasyAdminPlusBundle\Controller\AdminController;
 
 class FilterSubscriber implements EventSubscriberInterface
 {
+    
     private $session;
-    private $eaConfigManager;
+    
+    private $easyadmin_config_manager;
+    
     private $filterState;
-
-    public function __construct( Session $session, FilterState $filterState, $eaConfigManager )
+    
+    public function __construct(Session $session, FilterState $filterState, $easyadmin_config_manager)
     {
         $this->session = $session;
-        $this->eaConfigManager = $eaConfigManager;
+        $this->easyadmin_config_manager = $easyadmin_config_manager;
         $this->filterState = $filterState;
-
     }
-
+    
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
-        if ($request->attributes->get('_controller') != "Lle\EasyAdminPlusBundle\Controller\AdminController::indexAction") {
+        $controller = $request->attributes->get('_controller');
+        $class = strstr($controller, '::', true);
+        if (! is_subclass_of($class, AdminController::class)) {
             return;
         }
         if (null === $entityName = $event->getRequest()->query->get('entity')) {
             return;
         }
-
-        $entity = $this->eaConfigManager->getEntityConfiguration($entityName);
+        
+        $entity = $this->easyadmin_config_manager->getEntityConfiguration($entityName);
         if (isset($entity['filter'])) {
             $this->filterState->bindRequest($request, $entity);
         }
     }
-
+    
     public static function getSubscribedEvents()
     {
         return [
-           'kernel.request' => ['onKernelRequest', 0],
+            'kernel.request' => [
+                'onKernelRequest',
+                0
+            ]
         ];
     }
 }
