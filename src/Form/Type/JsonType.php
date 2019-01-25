369f6c212b11d94759e91b0f41fd1238390a3247
joris <joris@2le.net>
2019-01-25 12:14:04 +0100

    Type json compatible sf form 4.2

--- src/Form/Type/JsonType.php
+++ src/Form/Type/JsonType.php
@@ -2,7 +2,7 @@
 namespace Lle\EasyAdminPlusBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +12,7 @@ class JsonType extends AbstractType {
 
     public function getParent()
     {
-        return TextareaType::class;
+        return CollectionType::class;
     }
 
     public function getName()
