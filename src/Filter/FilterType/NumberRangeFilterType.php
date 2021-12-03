272a17c0fa384ab7b5c5dc9d2dc6d2e8b954a08e
manon <manon@2le.net>
2021-12-03 08:53:46 +0100

    Fix NumberRangeFilterType

--- src/Filter/FilterType/NumberRangeFilterType.php
+++ src/Filter/FilterType/NumberRangeFilterType.php
@@ -12,20 +12,22 @@ class NumberRangeFilterType extends AbstractFilterType
 
     public function apply($queryBuilder)
     {
-      if (isset($data['value'][0]) or isset($data['value'][1])) {
-        if($data['value'][0]){
-          $queryBuilder->andWhere($this->alias . $this->columnName .' >= :min_'.$this->uniqueId);
-          $queryBuilder->setParameter('min_'.$this->uniqueId, $data['value'][0]);
+        $data = $this->data;
+        if (isset($data['value'][0]) or isset($data['value'][1])) {
+            if ($data['value'][0]) {
+                $queryBuilder->andWhere($this->alias . $this->columnName . ' >= :min_' . $this->uniqueId);
+                $queryBuilder->setParameter('min_' . $this->uniqueId, $data['value'][0]);
+            }
+            if ($data['value'][1]) {
+                $queryBuilder->andWhere($this->alias . $this->columnName . ' <= :max_' . $this->uniqueId);
+                $queryBuilder->setParameter('max_' . $this->uniqueId, $data['value'][1]);
+            }
         }
-        if($data['value'][1]) {
-          $queryBuilder->andWhere($this->alias . $this->columnName .' <= :max_'.$this->uniqueId);
-          $queryBuilder->setParameter('max_'.$this->uniqueId, $data['value'][1]);
-        }
-      }
-      
+
     }
 
-    public function getTemplate(){
+    public function getTemplate()
+    {
         return '@LleEasyAdminPlus/filter/type/number_range_filter.html.twig';
     }
 
